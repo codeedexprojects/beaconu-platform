4a31d26e8d3678d09e5a2d6d92af2b87348c28cb
vinzencor <rahulpradeepan55@gmail.com>
2026-05-14 09:52:13 +0530

    fix(laning-lead): route missing added back

--- apps/api/src/routes/public/index.ts
+++ apps/api/src/routes/public/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import publicUniversityRoutes from "@/modules/universities/routes/public.routes";
 import publicBlogRoutes from "@/modules/content/routes/public.routes";
+import collegeOnboardingRoutes from "@/modules/landing-page/routes/public.routes";
 
 const router: Router = Router();
 
 router.use("/universities", publicUniversityRoutes);
 router.use("/blogs", publicBlogRoutes);
+router.use("/college-onboarding", collegeOnboardingRoutes);
 
 export default router;
